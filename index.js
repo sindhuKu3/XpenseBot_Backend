c37739232dbbea0ad6bd6d439cf7d0589808e13f
sindhuKu3 <sindhuku3@gmail.com>
2024-04-22 07:38:11 +0530

    fromeditorial

--- index.js
+++ index.js
@@ -17,20 +17,39 @@ app.use(cors());
 app.use(cookieParser()) ; 
 app.use(checkForAuthenticationCookie("token")) ;
 
-app.use(
-  cors({
-    origin: ["https://xpense-bot-frontend.vercel.app/"],
-    methods: ["POST", "GET"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: ["https://xpense-bot-frontend.vercel.app/"],
+//     methods: ["POST", "GET"],
+//     credentials: true,
+//   })
+// );
 app.use("/api/users" , userRoute) ;
 app.use("/api/transaction", transactionRoute);
 
 app.get("/",(req,res)=>{
   res.json("Hello");
 })
+//----deployement purpose-------------
+const __dirname1 = path.resolve();
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+
+
+
+
+//----deployement purpose-------------
 
 const server=()=>{
     db() ; 
